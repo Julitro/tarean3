d1421c71531911d420c981d1e9dae60867342d57
Usuario <Usuario@DESKTOP-CFT0RVT>
2017-08-09 17:31:34 -0600

    primeraparte

---
+++ src/tarea3commits/Tarea3commits.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea3commits;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Tarea3commits {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        new inicial ();
+        // TODO code application logic here
+    }
+    
+}
