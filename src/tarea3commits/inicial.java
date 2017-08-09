a49b93b9e5330027c2262369b13b11fb05be237c
Usuario <Usuario@DESKTOP-CFT0RVT>
2017-08-09 17:35:53 -0600

    segundaparte

--- src/tarea3commits/inicial.java
+++ src/tarea3commits/inicial.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
  */
 public class inicial {
     
-    public tt() {
+    public inicial() {
 
         menuPrincipal();
     }
@@ -55,5 +55,80 @@ public class inicial {
             menuPrincipal();
         }
     }
+     public void menuUsuarios() {
+        System.out.println("   Menu Usuarios");
+        System.out.println("1. Ingresar Usuarios");
+        System.out.println("2. Mostrar todos los Usuarios");
+        System.out.println("3. usuario personalizado");
+        System.out.println("4. Menu Principal");
+        System.out.println("5. Salir");
+        lector = new Scanner(System.in);
+        int numero ;
+        switch (numero = lector.nextInt()) {
+            case 1:
+               IngresarUsuario();
+                break;
+                
+            case 2:
+                    mostrarUsuario();
+               break;     
+               
+            case 3:
+                usuarioPersonalizado();     
+            break;
+            
+            case 4:
+                menuPrincipal();
+                break;
+            case 5:
+                            System.exit(0);
+                            break;
+            default:
+        }
+    }
+    private void IngresarUsuario() {
+        System.out.println("hola");
+        int t = 0;
+        while (t < 5) {
+            System.out.println("Ingresar Usuario:");
+            Scanner lector2 = new Scanner(System.in);
+            String nombre = lector2.nextLine();
+            nombres1[t] =nombre;
+            t++;
+        }
+        for (int l=0;l<5;l++){
+            System.out.println(nombres1[l]);
+                 menuUsuarios(); 
+        }   
+    }
+     public void usuarioPersonalizado(){
+         
+         System.out.println("ingrese un usuario");
+      
+        boolean encontrado = false;
+        Scanner S = new Scanner(System.in);
+        String nombreingresado = S.nextLine();
+        int s = 0;
+        while (s < nombres1.length && !encontrado) {
+            if (nombreingresado.equals(nombres1[s])) {
+                System.out.println(nombres1[s]);
+                encontrado = true;
+            }
+            s++;       
+            }
+        if (!encontrado) {
+            System.out.println("Usuario no encontrado");
+             menuUsuarios();
+     } 
+     }
+     
+            public void mostrarUsuario(){
+          System.out.println("Mostrar Todos los usuarios");
+                            for (int i = 0; i < 5; i++) {
+                                System.out.println( nombres1 [i]);}
+     menuUsuarios();         
+}
+public void menuPalindromas(){ 
     
+}
 }
